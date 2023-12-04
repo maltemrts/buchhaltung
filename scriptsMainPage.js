c6a95132c9cc47ff2ed55daf596c67bd742266d1
Oliverh0714 <152703745+Oliverha0714@users.noreply.github.com>
2023-12-04 11:57:43 +0100

    Delete scriptsMainPage.js

--- scriptsMainPage.js
+++
@@ -1,9 +0,0 @@
-function openPopup(popupId) {
-    var modal = document.getElementById(popupId);
-    modal.style.display = "block";
-}
-
-function closeModal() {
-    var modal = document.getElementById('newEntryModal');
-    modal.style.display = "none";
-}
